9c2b3c9930107c9eb5428248f0aa2c34c7e92442
github username <github email address>
2020-08-17 22:50:20 -0400

    added eventListener on uses and prefilled form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", e=>{
-    
-    
-    let studentUrl = "http://localhost:3000/students"
-    const ul = document.getElementById("students")
+       
+    const studentUrl = "http://localhost:3000/students"
+    const studentUl = document.getElementById("students")
     const meetingForm = document.getElementById("meeting_form")
     
     async function fetchStudents (){
@@ -14,44 +13,37 @@ document.addEventListener("DOMContentLoaded", e=>{
             div.dataset.id = `${student.id}`
             div.id = "rectangle"
             div.innerText = `${student.name}` 
-            ul.appendChild(div)
+            studentUl.appendChild(div)
         }) 
     }
 
     fetchStudents()
 
-    ul.addEventListener("click", e=>{
+    studentUl.addEventListener("click", e=>{
 
       let id = parseInt(e.target.dataset.id)
 
       async function fetchOneStudent (){
         let response = await fetch(studentUrl)
         let data = await response.json()
-        let host_student =  data.filter(student =>( student.id === id))
-        console.log(host_student[0])
+        let host_student =  data.filter(student =>(student.id === id))
+        // console.log(host_student[0])
 
         let hostZoomURL = host_student[0].zoom_meeting_url
         let hostMeetingId = host_student[0].zoom_meeting_id
         let hostMeetingPassword = host_student[0].zoom_meeting_password
 
-        // debugger
         meetingForm.display_name.value = hostZoomURL
         meetingForm.meeting_number.value = hostMeetingId
         meetingForm.meeting_pwd.value = hostMeetingPassword
 
 
             meetingForm.addEventListener("submit", e=>{
-        
-    
+          
             e.preventDefault()
 
              console.log("click form")
-    
-            // let name = form.display_name.value
-            // let meetingNumber = form.meeting_number.value
-            // let meetingPwd = form.meeting_pwd.value
-            // let meetingEmail = form.meeting_email.value
-      
+             //post request to API
 
             
             })//form
