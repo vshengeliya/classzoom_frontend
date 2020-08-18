c43779d0dbeb0183f2886a9459e8be1e88706c03
github username <github email address>
2020-08-17 22:47:57 -0400

    added eventListener on user+on Form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,71 @@
-let studentUrl = "http://localhost:3000/students"
+document.addEventListener("DOMContentLoaded", e=>{
+    
+    
+    let studentUrl = "http://localhost:3000/students"
+    const ul = document.getElementById("students")
+    const meetingForm = document.getElementById("meeting_form")
+    
+    async function fetchStudents (){
+        let response = await fetch(studentUrl)
+        let data = await response.json()
+     
+        data.forEach(student =>{
+            let div = document.createElement('div')
+            div.dataset.id = `${student.id}`
+            div.id = "rectangle"
+            div.innerText = `${student.name}` 
+            ul.appendChild(div)
+        }) 
+    }
 
-async function fetchStudents (){
-   let response = await fetch(studentUrl)
-   let data = await response.json()
-   data.forEach(student =>{
-       let li = document.createElement('li')
-       li.innerText = `Name: ${student.name}, Zoom: ${student.zoom_url}`
-       let ul = document.getElementById("students")
-       ul.appendChild(li)
-   })
-}
-fetchStudents()
+    fetchStudents()
+
+    ul.addEventListener("click", e=>{
+
+      let id = parseInt(e.target.dataset.id)
+
+      async function fetchOneStudent (){
+        let response = await fetch(studentUrl)
+        let data = await response.json()
+        let host_student =  data.filter(student =>( student.id === id))
+        console.log(host_student[0])
+
+        let hostZoomURL = host_student[0].zoom_meeting_url
+        let hostMeetingId = host_student[0].zoom_meeting_id
+        let hostMeetingPassword = host_student[0].zoom_meeting_password
+
+        // debugger
+        meetingForm.display_name.value = hostZoomURL
+        meetingForm.meeting_number.value = hostMeetingId
+        meetingForm.meeting_pwd.value = hostMeetingPassword
+
+
+            meetingForm.addEventListener("submit", e=>{
+        
+    
+            e.preventDefault()
+
+             console.log("click form")
+    
+            // let name = form.display_name.value
+            // let meetingNumber = form.meeting_number.value
+            // let meetingPwd = form.meeting_pwd.value
+            // let meetingEmail = form.meeting_email.value
+      
+
+            
+            })//form
+        
+         }//fetchOne Student
+         fetchOneStudent()
+    })//evenListener
+
+
+
+
+
+
+
+
+
+})//Content Loaded
