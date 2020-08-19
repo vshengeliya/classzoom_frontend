c029d10e0e1a3d6abff1f68f39f81302a0e75670
github username <github email address>
2020-08-18 22:13:45 -0400

    dropdown button html

--- src/classroom.js
+++ src/classroom.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", e=>{
     document.styleSheets[53].disabled = true;
-
+    
     const studentUrl = "http://localhost:3000/students"
     const meetingsUrl = "http://localhost:3000/meetings"
     const studentUl = document.getElementById("students")
     const navBar = document.querySelector("#nav-tool")
+    const bodyHTML = document.getElementsByTagName('body')[0]
     
     navBar.hidden = true
     
     function welcomeUser(){
-            let welcomeForm = document.querySelector("#welcome")
+        // debugger
+        let welcomeForm = document.querySelector("#welcome")
     
          welcomeForm.innerHTML =`
          <div class="container">
@@ -46,6 +48,26 @@ document.addEventListener("DOMContentLoaded", e=>{
         
              welcomeForm.remove()
              navBar.hidden = false
+            //  let userButton = document.createElement('button')
+            //   userButton.innerText="User Profile"
+            //   userButton.className='btn btn-info pull-left'
+            //   bodyHTML.appendChild(userButton)
+
+               let userDropdown = document.createElement('div')
+
+               userDropdown.innerHTML=`
+               <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      User Info
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" href="#">Update Name</a>
+                      <a class="dropdown-item" href="#">Update email</a>
+                      <a class="dropdown-item" href="#">Delete User</a>
+                    </div>
+                </div>
+               `
+               bodyHTML.appendChild(userDropdown)
     
              let body = {
                 name: name,
@@ -54,7 +76,8 @@ document.addEventListener("DOMContentLoaded", e=>{
                 // zoom_meeting_password:null,
                 // zoom_meeting_time: null,
                 // zoom_meeting_length: null      
-             }  
+                }  
+              
              const options = {
                      method: 'POST',
                      headers: {
@@ -64,31 +87,32 @@ document.addEventListener("DOMContentLoaded", e=>{
                      body: JSON.stringify(body)
                      }  
                      fetch(meetingsUrl, options)
+                     
             
-                 })//welcomeForm eventL
-            }//f welcomeUser
-            
-        welcomeUser()
+                    })//welcomeForm eventL
+                }//f welcomeUser
+                
+                welcomeUser()
+                
+                fetchStudents()
+                    async function fetchStudents (){
+                        let response = await fetch(studentUrl)
+                        let data = await response.json()
+                        
+                        console.log(data)
+                        
+                        data.forEach(student =>{
+                            let div = document.createElement('div')
+                            div.dataset.id = `${student.id}`
+                            div.id = "rectangle"
+                            div.innerText = `${student.name}` 
+                            studentUl.appendChild(div)
+                        }) 
+                    }
+
             
     // navBar.hidden = false
     
-    async function fetchStudents (){
-        // debugger
-        let response = await fetch(studentUrl)
-        let data = await response.json()
-        
-        console.log(data)
-        
-        data.forEach(student =>{
-            let div = document.createElement('div')
-            div.dataset.id = `${student.id}`
-            div.id = "rectangle"
-            div.innerText = `${student.name}` 
-            studentUl.appendChild(div)
-        }) 
-    }
-    
-    fetchStudents()
     
     studentUl.addEventListener("click", e=>{
         const meetingForm = document.getElementById("meeting_form")
@@ -101,12 +125,12 @@ document.addEventListener("DOMContentLoaded", e=>{
         let host_student =  data.filter(student =>( student.id === id))
         console.log(host_student[0])
 
-        let hostZoomURL = host_student[0].zoom_meeting_url
+        let hostZoomName = host_student[0].name
         let hostMeetingId = host_student[0].zoom_meeting_id
         let hostMeetingPassword = host_student[0].zoom_meeting_password
         
         // debugger
-        meetingForm.display_name.value = hostZoomURL
+        meetingForm.display_name.value = hostZoomName
         meetingForm.meeting_number.value = hostMeetingId
         meetingForm.meeting_pwd.value = hostMeetingPassword
               
