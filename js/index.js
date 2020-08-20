cf12bcb7051be523048fbcb9bdd702fefedbf7a7
kylejb <kjburda@gmail.com>
2020-08-20 12:56:48 -0400

    Removed require('dotenv') due to error, which preventing iFrame from rendering; resolve API key management after MVP

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 (function () {
 
   var testTool = window.testTool;
@@ -15,14 +13,14 @@ require('dotenv').config();
   // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
   ZoomMtg.preLoadWasm();
 
-  var API_KEY = process.env.API_KEY;
+  var API_KEY = "process.env.API_KEY";
 
   /**
    * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
    * The below generateSignature should be done server side as not to expose your api secret in public
    * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/web/essential/signature
    */
-  var API_SECRET = process.env.API_SECRET;
+  var API_SECRET = "process.env.API_SECRET";
 
   // some help code, remember mn, pwd, lang to cookie, and autofill.
   document.getElementById("display_name").value =
